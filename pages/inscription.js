b2dddfe91f3be7c7df6e22fc4f0d616186132918
Claire Vaney <clairevan.54.cv@gmail.com>
2020-10-04 18:21:29 +0200

    added css base form inscription

--- pages/inscription.js
+++ pages/inscription.js
@@ -24,35 +24,50 @@ const InscriptionPage = () => {
 
   return (
     <Layout>
-      <div className="form">
-        <div>
-          <label htmlFor="name">Prénom</label>
-          <input type="name" />
-        </div>
-        <div>
+      <div className="container">
+        <div className="form">
+          <div>
+            <label htmlFor="lastname">Nom</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="firstname">Prénom</label>
+            <input type="text" />
+          </div>
           <div>
             <label htmlFor="number">Numéro de téléphone</label>
-            <input type="number" />
+            <input type="text" />
           </div>
-          <label htmlFor="name">Email</label>
-          <input type="name" />
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="email" />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input type="password" />
+          </div>
         </div>
-        <div>
-          <label htmlFor="firstname">Password</label>
-          <input type="firstname" />
-        </div>
-        <div>
+        <div className="button">
           <button onClick={register}>Bienvenue</button>
         </div>
       </div>
       <style jsx>{`
-        .form {
-          display: flex;
-          flex-direction: column;
+        .container {
+          width: 44rem;
           margin: auto;
+        }
+        .form {
+          display: grid;
+          flex-direction: column;
+          grid-template-columns: repeat(2, minmax(0, 1fr));
         }
         .form div {
           display: flex;
+          flex-direction: column;
+          margin: 0.5rem;
+        }
+        .button {
+          margin: auto;
         }
       `}</style>
     </Layout>
