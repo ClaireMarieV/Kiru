ca6f7532380d44da72166fcc033f5f0dc8976d46
Claire Vaney <clairevan.54.cv@gmail.com>
2020-12-16 09:31:17 +0100

    deleted pages/admin clothes and modifiex index

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -2,20 +2,5 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
 
-const IndexPage = () => (
-  <Layout>
-    <div className="logo">
-      <img src="/logo/kiru.svg" />
-      <style jsx>{`
-        .logo {
-          margin: auto;
-          max-width: 25rem;
-        }
-        .logo img {
-          width: 100%;
-        }
-      `}</style>
-    </div>
-  </Layout>
-);
+const IndexPage = () => <Layout></Layout>;
 export default IndexPage;
