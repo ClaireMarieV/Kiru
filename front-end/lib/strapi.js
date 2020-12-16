55bafbf04fc4692e8f894a89e8403a411831110d
Claire Vaney <clairevan.54.cv@gmail.com>
2020-12-16 09:29:42 +0100

    added fetch with url connected strapi

--- front-end/lib/strapi.js
+++ front-end/lib/strapi.js
@@ -2,3 +2,7 @@ exports.getClothes = () =>
   fetch(process.env.NEXT_PUBLIC_STRAPI_URL + "/clothes").then((response) =>
     response.json()
   );
+exports.getClothe = (id) =>
+  fetch(
+    process.env.NEXT_PUBLIC_STRAPI_URL + "/clothes/" + id
+  ).then((response) => response.json());
